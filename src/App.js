4289ed2dda61ed4e3add1c2d8c99c441b85dd867
aliaksandr vishneuski <aliaksandr_vishneuski@senla.eu>
2019-08-20 18:05:52 +0300

    useContext -before Hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css'
 import DataFetching from './components/DataFetching';
+import ComponentA from './components/ComponentA';
 
-
+export const NameContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
       <DataFetching />
+      <NameContext.Provider value = {"Alex"}>
+        <ComponentA /> 
+      </NameContext.Provider>
+
      </div>
   );
 }
---
+++ src/components/ComponentB.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import {NameContext} from '../App'
+
+function ComponentB() {
+    return (
+        <div>
+            <NameContext.Consumer>
+	            {
+                name => {
+                           return  <p> Current name: {name} </p>
+                        }
+                }
+            </NameContext.Consumer>
+        </div>
+        )
+    }
+
+export default ComponentB
