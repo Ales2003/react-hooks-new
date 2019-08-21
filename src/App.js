f1e3d603ff771cfb8ca21878e6d02a0b9839934d
aliaksandr vishneuski <aliaksandr_vishneuski@senla.eu>
2019-08-21 12:06:25 +0300

    useReducer -  multiple reducer local

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css'
-import ReduceCounterStateActionObject from './components/ReduceCounterStateActionObject';
+import ReduceCounterMultipleReducers from './components/ReduceCounterMultipleReducers';
 
 export const NameContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-        <ReduceCounterStateActionObject />
+        <ReduceCounterMultipleReducers />
      </div>
   );
 }
---
+++ src/components/ReduceCounterMultipleReducers.js
@@ -0,0 +1,51 @@
+
+import React, {useReducer} from 'react'
+
+//2. define args for useReducer outside 
+//2.1. define initialState
+const initialState = 0
+
+//2.2. define currentState and action params
+//2.2.1. currentState
+//2.2.2. CompositionEvent action - instruction to the reducer function for perform state transition
+const reducer = (state, action) => {
+    //2.3. return newState with switch
+    switch(action){
+        //add
+        case 'increment':
+            return state + 1
+            //substract
+        case 'decrement':
+            return state - 1
+        case 'reset':
+            return initialState
+        default:
+            return state
+    }
+ }
+
+function ReduceCounterMultipleReducers() {
+    //1. describe function useReducer(reducer, initialState)
+    //3, count - current state 
+    //3. dispatch - function - allows execute code correspond to particular action
+    //argument for dispatch method  is action specified in reducer f-n
+    const [count, dispatch] = useReducer(reducer, initialState)
+    const [countTwo, dispatchTwo] = useReducer(reducer, initialState)
+
+    return (
+        <div>
+            <div>Count - {count}</div>
+            <button onClick={() => dispatch('increment')}>Increment</button>
+            <button onClick={() => dispatch('decrement')}>Decrement</button>
+            <button onClick={() => dispatch('reset')}>Reset</button>
+            <div>
+                <div>Count - {countTwo}</div>
+                <button onClick={() => dispatchTwo('increment')}>Increment</button>
+                <button onClick={() => dispatchTwo('decrement')}>Decrement</button>
+                <button onClick={() => dispatchTwo('reset')}>Reset</button>
+        </div>
+        </div>
+        )
+    }
+
+export default ReduceCounterMultipleReducers
