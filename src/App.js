9a40d5a00b2520bba571caaeb827b52ee4093dca
aliaksandr vishneuski <aliaksandr_vishneuski@senla.eu>
2019-08-21 10:58:44 +0300

    useReducer - first simple reducer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import './App.css'
-import DataFetching from './components/DataFetching';
-import ComponentA from './components/ComponentA';
+import ReducerCounter from './components/ReducerCounter';
 
 export const NameContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <DataFetching />
-      <NameContext.Provider value = {"Alex"}>
-        <ComponentA /> 
-      </NameContext.Provider>
-
+      <ReducerCounter />
      </div>
   );
 }
